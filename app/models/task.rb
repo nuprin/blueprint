4f9a0930e64e681e42c65f927e1803cf56729167
Kristjan <kristjan@causes.com>
2008-12-03 15:58:59 -0800

    Task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,12 @@
 class Task < ActiveRecord::Base
+  belongs_to :project
+  belongs_to :creator, :class_name => 'User'
+  belongs_to :asignee, :class_name => 'User'
+
+  validates_presence_of :creator
+
+  validates_numericality_of :estimate, :less_than_or_equal_to => 20
+
+  validates_length_of :title, :in => [1...255]
+  validates_length_of :description, :maximum => 5000
 end
