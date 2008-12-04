513cd05438f07c594890a8687c951f422f5f6505
Kristjan <kristjan@causes.com>
2008-12-03 16:16:48 -0800

    Wire User/Project has_many Tasks

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
+  has_many :task_list_items, :as => :context
+  has_many :tasks
+
   validates_length_of :title, :in => 1...255
   validates_length_of :description, :maximum => 5000, :allow_nil => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :task_list_items, :as => :context
+  has_many :tasks
+
   validates_length_of :name, :in => 1...50
   validates_numericality_of :fbuid, :allow_nil => true
 end
