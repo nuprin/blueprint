1cd7bc77343427a32929ff5c9e48ef0d932476cd
Chris <chris@causes.com>
2008-12-03 21:14:00 -0800

    Stub out people template.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :tasks
 
   map.root :controller => "tasks", :action => "index"
 
   map.resources :tasks,
-                :collection => {:reorder => :post}
+                :collection => {:people => :get, :reorder => :post}
 
 end
