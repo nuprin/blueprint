8e047c66224cb9bb3b4b8e0b6263f79faf5f2731
Kristjan <kristjan@causes.com>
2008-12-03 16:02:58 -0800

    List item validation

--- app/models/task_list_item.rb
+++ app/models/task_list_item.rb
@@ -1,3 +1,10 @@
 class TaskListItem < ActiveRecord::Base
   acts_as_list
+
+  belongs_to :task
+  belongs_to :context, :polymorphic => true
+
+  validates_presence_of :task
+  validates_presence_of :context
+  validates_numericality_of :position
 end
