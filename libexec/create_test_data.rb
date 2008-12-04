9088ad8ab4edd5c171b1de7b18e502e4f142aa2d
Brad Fults <bfults@gmail.com>
2008-12-03 21:04:35 -0800

    Add another project to the test data.

--- libexec/create_test_data.rb
+++ libexec/create_test_data.rb
@@ -5,37 +5,46 @@ TaskListItem.destroy_all
 
 u1 = User.create! :name => "Brad"
 u2 = User.create! :name => "Kristján"
-p1 = Project.create! :title => "Petitions",
+projects = []
+projects << Project.create!(:title => "Petitions",
                      :description => %(A feature that lets people sign their
                      name on various petitions that nonprofits will deliver to
                      individuals or organizations in a position to effect
-                     change.).squish
+                     change.).squish)
 
-task_opts = {
-  :kind => 'task',
-  :status => 'assigned',
-  :project => p1,
-  :creator => u1,
-  :assignee => u2
-}
+projects << Project.create!(:title => "Birthday Wish",
+                     :description => %(A feature that lets people get
+                     donations on their birthday.).squish)
 
-tasks = []
-tasks << Task.create!(task_opts.merge(
-  :title => "Add link tracking to announcements", :estimate => 3,
-  :due_date => Date.today + rand(90)))
-tasks << Task.create!(task_opts.merge(
-  :title => "Display feature stats at top of stats page", :estimate => 10,
-  :due_date => Date.today + rand(90)))
-tasks << Task.create!(task_opts.merge(
-  :title => "Allow site admins to remove petitions from causes",
-  :estimate => 1, :due_date => Date.today + rand(90)))
+projects.each do |p|
 
-task_opts[:creator], task_opts[:assignee] =
-task_opts[:assignee], task_opts[:creator]
+  task_opts = {
+    :kind => 'task',
+    :status => 'assigned',
+    :project => p,
+    :creator => u1,
+    :assignee => u2
+  }
 
-tasks << Task.create!(task_opts.merge(
-  :title => "Implement 'Email All Signers' functionality",
-  :due_date => Date.today + rand(90)))
-tasks << Task.create!(task_opts.merge(
-  :title => "Track status messages with Uhura", :estimate => 5,
-  :due_date => Date.today + rand(90)))
+  tasks = []
+  tasks << Task.create!(task_opts.merge(
+    :title => "Add link tracking to announcements", :estimate => 3,
+    :due_date => Date.today + rand(90)))
+  tasks << Task.create!(task_opts.merge(
+    :title => "Display feature stats at top of stats page", :estimate => 10,
+    :due_date => Date.today + rand(90)))
+  tasks << Task.create!(task_opts.merge(
+    :title => "Allow site admins to remove petitions from causes",
+    :estimate => 1, :due_date => Date.today + rand(90)))
+
+  task_opts[:creator], task_opts[:assignee] =
+  task_opts[:assignee], task_opts[:creator]
+
+  tasks << Task.create!(task_opts.merge(
+    :title => "Implement 'Email All Signers' functionality",
+    :due_date => Date.today + rand(90)))
+  tasks << Task.create!(task_opts.merge(
+    :title => "Track status messages with Uhura", :estimate => 5,
+    :due_date => Date.today + rand(90)))
+
+end
