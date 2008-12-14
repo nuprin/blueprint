3a06a1cced03a749a4e8b2ea45808810d68ccae2
Chris <chris@causes.com>
2008-12-14 01:20:28 -0800

    Start using a local and stable version of sortable.

--- public/javascripts/tasks/index.js
+++ public/javascripts/tasks/index.js
@@ -2,17 +2,19 @@ var Tasks = {
   makeSortable: function() {
     $(document.body).sortable({
       axis:  'y',
-      items: 'table.task_list tbody > tr',
+      items: 'table.task_list tbody>tr',
       update: function (evt, ui) {
       $.post("/tasks/reorder", {
         list_item_id: ui.item.attr('id').replace(/list_item_/, ''),
         list_item_position: ui.item.prevAll().length + 1
       },
-      function (data) {}, "json");
+      function (data) {
+        
+      }, "json");
       }
     });
   }
 };
-$(document).ready(function() {
+$(function() {
   Tasks.makeSortable();
 });
