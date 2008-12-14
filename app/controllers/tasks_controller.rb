63fe4cab0a8fca116025237fefc9154b73bba224
Chris <chris@causes.com>
2008-12-13 18:09:08 -0800

    * Drag and drop support for all task lists.  * Change redirection after project creation.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,10 @@ class TasksController < ApplicationController
     flash[:notice] = "&ldquo;#{task.title}&rdquo; created."
     if params[:commit] == "Create and Add Another"
       redirect_to new_task_url(:kind => params[:task][:kind])
+    elsif task.project_id
+      redirect_to project_url(task.project_id)
+    elsif task.assignee_id
+      redirect_to user_url(task.assignee_id)
     else
       redirect_to tasks_url
     end
--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -3,3 +3,23 @@ class String
     self.last == 's' ? "#{self}'" : "#{self}'s"
   end
 end
+
+module ActiveSupport::Inflector
+  # Dehumanize turns some human readable sentence into an underscored legal
+  # instance variable.
+  def dehumanize(word)
+    word.tr(' ', '_').delete('^a-zA-Z0-9_').downcase
+  end
+end
+
+module ActiveSupport
+  module CoreExtensions
+    module String
+      module Inflections
+        def dehumanize
+          Inflector.dehumanize(self)
+        end
+      end
+    end
+  end
+end
