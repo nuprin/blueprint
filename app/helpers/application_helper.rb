8bb4b10968d4bd2a01f4abf74f8a3487b860208a
Chris <chris@causes.com>
2008-12-14 17:11:12 -0800

    * Mark complete button.  * Helper methods for user and project linking.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def link_to_user(user)
+    link_to user.name, user_path(user)
+  end
+  def link_to_project(project)
+    link_to project.title, project_path(project)
+  end
 end
