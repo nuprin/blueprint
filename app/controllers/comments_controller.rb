02ed23f1a7536d93cede87768d7c2709b09a3da4
Chris <chris@causes.com>
2008-12-15 16:45:47 -0800

    Comment deletion support.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,10 @@ class CommentsController < ApplicationController
     flash[:notice] = "Your comment has been created."
     redirect_to task_path(comment.task_id)
   end
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    flash[:notice] = "Your comment has been deleted."
+    redirect_to task_path(comment.task_id)
+  end
 end
