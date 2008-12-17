0e218d004d587922878fd89542b73fd75bdd968a
Chris <chris@causes.com>
2008-12-17 12:11:24 -0800

    Styling for comment adding. BIG photo.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :author, :class_name => "User"
 
 has_attached_file :image,
-  :styles => { :small => "150x150>" },
+  :styles => { :large => "360x360>" },
   :url  => "/assets/comments/:id/:style/:basename.:extension",
   :path => ":rails_root/public/assets/comments/:id/:style/:basename.:extension"
 end
