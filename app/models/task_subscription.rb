54929869da3e7bcce4891ad7f6980b35866aacd7
Kevin Ball <kball@kevin-balls-macbook-pro.local>
2008-12-19 12:00:27 -0800

    Require user & task for task subscription

--- app/models/task_subscription.rb
+++ app/models/task_subscription.rb
@@ -1,5 +1,8 @@
 class TaskSubscription < ActiveRecord::Base
   belongs_to :user
   belongs_to :task
+  validates_presence_of :user
+  validates_presence_of :task
   validates_uniqueness_of :user_id, :scope => [:task_id]
+  
 end
