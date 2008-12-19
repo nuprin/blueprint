f7920b238138d40dac2f8485d4bdf40e353384d9
Chris <chris@causes.com>
2008-12-19 15:59:43 -0800

    Cleaner redirection.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,12 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.create!(params[:comment])
     flash[:notice] = "Your comment has been created."
-    redirect_to task_path(comment.task_id)
+    redirect_to comment.task
   end
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
     flash[:notice] = "Your comment has been deleted."
-    redirect_to task_path(comment.task_id)
+    redirect_to comment.task
   end
 end
