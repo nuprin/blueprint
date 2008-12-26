b35db07753414101147ec6abb6c9dfae457bd7f4
Chris <chris@causes.com>
2008-12-26 03:21:20 -0800

    Basic search results for tasks.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,9 @@ class SearchesController < ApplicationController
 
     if q =~ /^\d+$/
       t = Task.find_by_id(params[:q])
-      redirect_to task_url(t) and return if t
+      redirect_to task_path(t) and return if t
     end
 
-    search_results = [Project, Task].map do |model|
-      [model.name, model.find_using_term(q).map(&:record)]
-    end
-    @all_results = Hash[*search_results.flatten(1)]
+    @results = Task.find_using_term(q).map(&:record)
   end
 end
