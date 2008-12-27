ed0d09da06ca555480fe7558cd49d7536295679c
Chris <chris@causes.com>
2008-12-26 20:52:34 -0800

    Functional tests for ProjectsController.

--- test/functional/projects_controller_test.rb
+++ test/functional/projects_controller_test.rb
@@ -1,35 +1,32 @@
 require 'test_helper'
 
-# TODO [chris]: Figure out how to stub out logging in in tests.
-
 class ProjectsControllerTest < ActionController::TestCase
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:projects)
-  # end
-  # 
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
-  # 
-  # test "should create projects" do
-  #   assert_difference('Project.count') do
-  #     post :create, :projects => {:title => "New Project"}
-  #   end
-  # 
-  #   assert_redirected_to projects_path(assigns(:projects))
-  # end
-  # 
-  # test "should show projects" do
-  #   get :show, :id => projects(:one).id
-  #   assert_response :success
-  # end
-  # 
-  # test "should update projects" do
-  #   put :update, :id => projects(:one).id, :projects => { }
-  #   assert_redirected_to projects_path(assigns(:projects))
-  # end
+  def setup
+    @controller.stubs(:viewer).returns(users(:one))
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:projects)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should create projects" do
+    assert_difference('Project.count') do
+      post :create, :project => {:title => "New Project"}
+    end
+  
+    assert_redirected_to project_path(assigns(:project))
+  end
+
+  test "should show projects" do
+    get :show, :id => projects(:one).id
+    assert_response :success
+  end
 
 end
