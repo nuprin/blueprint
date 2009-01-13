d46375e0da26c0d0252f3b246579e113789f280c
Chris <chris@causes.com>
2009-01-13 10:40:03 -0800

    Grrr...bobo make me angry.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def user_tabs(user)
     links = [
-      ["PRIORITIZED (#{user.task_list.size})", user_url(user)],
-      ["CARELOG", completed_user_url(user)],
-      ["PARKED", parked_user_url(user)]
+      ["PRIORITIZED (#{user.task_list.size})", user_path(user)],
+      ["CARELOG", completed_user_path(user)],
+      ["PARKED", parked_user_path(user)]
     ]
     links.map do |name, url|
       class_name = current_page?(url) ? "current" : ""
