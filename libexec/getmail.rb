b96a9d89827babf2d56b17ecc63d613e761d9af1
Kristjan <kristjan@causes.com>
2009-01-14 18:14:52 -0800

    Ignore messages without a Task ID

--- libexec/getmail.rb
+++ libexec/getmail.rb
@@ -18,35 +18,35 @@ messages = connection.search(['ALL'])
 fetch_params = ['ENVELOPE', 'BODY[1]']
 
 messages.each do |message_id|
-    # This will come in an array, of length 1 and raw_msg is FetchData.
-    raw_msg = connection.fetch(message_id,fetch_params)[0]
-    envelope = raw_msg.attr["ENVELOPE"]
-    
-    from_struct = envelope.from[0]
-    from_address = from_struct.mailbox + "@" + from_struct.host
+  # This will come in an array, of length 1 and raw_msg is FetchData.
+  raw_msg = connection.fetch(message_id,fetch_params)[0]
+  envelope = raw_msg.attr["ENVELOPE"]
+  
+  from_struct = envelope.from[0]
+  from_address = from_struct.mailbox + "@" + from_struct.host
 
-    # TODO: here we could crunch through to get out the ID from the reply-to 
-    # address.
+  # TODO: here we could crunch through to get out the ID from the reply-to 
+  # address.
 =begin
-    recipients = envelope.to
-    recipients.each { |r|
-        if r.mailbox.index("+").nil?
-            reply_to_task = Integer(r.mailbox.split["+"][1])
-    }
+  recipients = envelope.to
+  recipients.each { |r|
+      if r.mailbox.index("+").nil?
+          reply_to_task = Integer(r.mailbox.split["+"][1])
+  }
 =end    
 
-    puts from_address
-    # Need to catch if the email isn't found!
-    user = User.find_by_email(from_address)
-    
-    date_string = envelope.date
-    
-    subj_string = envelope.subject
-    task_id_string = subj_string.match('#\d+')[0]
-
-    puts task_id_string
-
-    task_id = Integer(task_id_string.split("#")[1])
+  puts from_address
+  # Need to catch if the email isn't found!
+  user = User.find_by_email(from_address)
+  
+  date_string = envelope.date
+  
+  subj_string = envelope.subject
+  # Only look at messages with Task IDs
+  # TODO: Do we want to just delete the rest?
+  if subj_string =~ /\(#(\d+)\)/
+    task_id = $1.to_i
+    puts task_id
     
     raw_body = raw_msg.attr["BODY[1]"]
     comment = remove_quotation(raw_body)
@@ -60,6 +60,7 @@ messages.each do |message_id|
     
     # Delete the message when we're done with it
     connection.store(message_id, "FLAGS", [:Deleted])
+  end
 end
 
 connection.logout
