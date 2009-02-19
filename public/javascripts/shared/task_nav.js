f28cae7a44f184e2f42b632656baba7003548de6
Kristjan <kristjan@causes.com>
2009-02-19 11:08:47 -0800

    Key navigation! j/k and Enter

---
+++ public/javascripts/shared/task_nav.js
@@ -0,0 +1,41 @@
+var TaskNavigation = {
+  setup: function() {
+    $(document.body).shortkeys({
+      'j': function() {
+        cursor = $("#main_task_list .cursor");
+        if (0 == cursor.length) {
+          $("#main_task_list tbody tr:first-child").addClass('cursor');
+          return;
+        }
+        next = cursor.next();
+        if (0 < next.length) {
+          cursor.removeClass('cursor');
+          next.addClass('cursor');
+        }
+      },
+      'k': function() {
+        cursor = $("#main_task_list .cursor");
+        if (0 == cursor.length) {
+          $("#main_task_list tbody tr:first-child").addClass('cursor');
+          return;
+        }
+        prev = cursor.prev();
+        if (0 < prev.length) {
+          cursor.removeClass('cursor');
+          prev.addClass('cursor');
+        }
+      },
+      'Enter': function() {
+        url = $(".cursor td:eq(1) a");
+        if (0 < url.length) {
+          location.href = url.get(0).href
+        }
+      }
+    }, {moreKeys: {'Enter':13} })
+  }
+}
+
+$(function() {
+  TaskNavigation.setup();
+  //$(document).keydown(function(e){alert(e.keyCode);});
+});
