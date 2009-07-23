9fa7efaf55e168f96f7e105ae67bb0b9745e4cd7
Brad Fults <brad@causes.com>
2009-07-23 09:52:45 -0700

    Clean up whitespace.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,19 +4,19 @@ module ApplicationHelper
   def link_to_user(user)
     link_to user.name, user_path(user)
   end
-  
+
   def link_to_initiative(initiative)
     link_to initiative.title, project_path(initiative)
   end
-  
+
   def link_to_project(project)
     link_to project.title, project_path(project)
   end
-  
+
   def link_to_task(task)
     link_to task.title, task_path(task)
   end
-  
+
   # Order matters!  auto_link links things inside tags
   def format_text(text)
     text = nl2br(text)
@@ -35,24 +35,24 @@ module ApplicationHelper
       link_to task, task_path($1)
     end
   end
-  
+
   def auto_link_commits(text)
     text.gsub /git:\s*[a-f0-9]+/i do |key|
       hash = key.split(':').last.strip
       link_to key, git_path(hash), :target => :blank
     end
   end
-  
+
   def nl2br(text)
     text.gsub(/\r\n?/, "\n").gsub(/\n/, '<br />')
   end
-  
+
   def if_any(collection)
     if collection.any?
       yield collection
     end
   end
-  
+
   def with_layout(layout_name, locals = {}, &block)
     render_content = {
       :layout => layout_name, :text => capture(&block), :locals => locals
@@ -60,7 +60,7 @@ module ApplicationHelper
     content = render(render_content)
     concat(content, block.binding)
   end
-  
+
   def with_locals(locals, &block)
     with_options(:locals => locals, &block)
   end
@@ -73,7 +73,7 @@ module ApplicationHelper
     @render_counts[partial] += 1
     content
   end
-  
+
   def tabs(links)
     render :partial => "/shared/tabs", :locals => {:links => links}
   end
